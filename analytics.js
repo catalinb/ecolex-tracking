e85da837522c6789028fc8acd3a8308662925391
Catalin Badea <catalin.badea392@gmail.com>
2015-01-16 15:09:27 +0200

    add ga tracking code.

--- analytics.js
+++ analytics.js
@@ -3,6 +3,14 @@ window.addEventListener('load', function () {
   // your code here
   console.log('loading: google-analytics-eaudeweb');
 
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-58379026-1', 'auto');
+  ga('send', 'pageview');
+
   setup_handlers();
 }, false);
 
